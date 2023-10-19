6fecd5d17103823acdd60e94c15ca6b49924dbdd
“MoriKeigo1213” <keigo.mori@excite.jp>
2023-10-19 18:00:31 +0900

    ADD セッション管理を削除しmysqlと接続

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"database/sql"
+	"strconv"
+	"sync"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	_ "github.com/go-sql-driver/mysql"
-	"log"
 )
 
 type Todo struct {
@@ -13,21 +13,11 @@ type Todo struct {
 	Text string `json:"text"`
 }
 
-var db *sql.DB
+var todos = make([]Todo, 0)
+var idCounter int
+var todoLock sync.Mutex
 
 func main() {
-	var err error
-	db, err = sql.Open("mysql", "user:password@tcp(mysql:3306)/todo_db?parseTime=True")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-	err = db.Ping()
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	r := gin.Default()
 	r.Use(cors.Default())
 
@@ -39,6 +29,7 @@ func main() {
 	r.Run(":8080")
 }
 
+// バリデーション
 func bindTodo(c *gin.Context) (Todo, error) {
 	var todo Todo
 	if err := c.ShouldBindJSON(&todo); err != nil {
@@ -49,30 +40,14 @@ func bindTodo(c *gin.Context) (Todo, error) {
 }
 
 func getTodos(c *gin.Context) {
-	todos, err := fetchTodos()
-	if err != nil {
-		c.JSON(500, gin.H{"error": "Internal server error"})
-		return
-	}
-	c.JSON(200, todos)
+	c.JSON(200, fetchTodos())
 }
 
-func fetchTodos() ([]Todo, error) {
-	rows, err := db.Query("SELECT todo_id, todo_text FROM todos")
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
+func fetchTodos() []Todo {
+	todoLock.Lock()
+	defer todoLock.Unlock()
 
-	var todos []Todo
-	for rows.Next() {
-		var todo Todo
-		if err := rows.Scan(&todo.ID, &todo.Text); err != nil {
-			return nil, err
-		}
-		todos = append(todos, todo)
-	}
-	return todos, rows.Err()
+	return todos
 }
 
 func addTodo(c *gin.Context) {
@@ -81,25 +56,17 @@ func addTodo(c *gin.Context) {
 		return
 	}
 
-	newTodo, err := addAndFetchTodo(todo)
-	if err != nil {
-		c.JSON(500, gin.H{"error": "Internal server error"})
-		return
-	}
-	c.JSON(201, newTodo)
+	c.JSON(201, addAndFetchTodo(todo))
 }
 
-func addAndFetchTodo(todo Todo) (Todo, error) {
-	result, err := db.Exec("INSERT INTO todos (todo_text) VALUES (?)", todo.Text)
-	if err != nil {
-		return Todo{}, err
-	}
-	id, err := result.LastInsertId()
-	if err != nil {
-		return Todo{}, err
-	}
-	todo.ID = int(id)
-	return todo, nil
+func addAndFetchTodo(todo Todo) Todo {
+	todoLock.Lock()
+	defer todoLock.Unlock()
+
+	idCounter++
+	todo.ID = idCounter
+	todos = append(todos, todo)
+	return todo
 }
 
 func updateTodo(c *gin.Context) {
@@ -108,11 +75,7 @@ func updateTodo(c *gin.Context) {
 		return
 	}
 
-	updatedTodo, found, err := updateAndFetchTodo(c.Param("id"), todo)
-	if err != nil {
-		c.JSON(500, gin.H{"error": "Internal server error"})
-		return
-	}
+	updatedTodo, found := updateAndFetchTodo(c.Param("id"), todo)
 	if found {
 		c.JSON(200, updatedTodo)
 	} else {
@@ -120,31 +83,48 @@ func updateTodo(c *gin.Context) {
 	}
 }
 
-func updateAndFetchTodo(id string, updatedTodo Todo) (Todo, bool, error) {
-	_, err := db.Exec("UPDATE todos SET todo_text = ? WHERE todo_id = ?", updatedTodo.Text, id)
+func updateAndFetchTodo(id string, updatedTodo Todo) (Todo, bool) {
+	todoLock.Lock()
+	defer todoLock.Unlock()
+
+	idInt, err := strconv.Atoi(id)
 	if err != nil {
-		return Todo{}, false, err
+		return Todo{}, false
 	}
-	row := db.QueryRow("SELECT todo_id, todo_text FROM todos WHERE todo_id = ?", id)
-	var todo Todo
-	if err := row.Scan(&todo.ID, &todo.Text); err != nil {
-		if err == sql.ErrNoRows {
-			return Todo{}, false, nil
+
+	for index, todo := range todos {
+		if todo.ID == idInt {
+			todos[index].Text = updatedTodo.Text
+			return todos[index], true
 		}
-		return Todo{}, false, err
 	}
-	return todo, true, nil
+
+	return Todo{}, false
 }
 
 func deleteTodo(c *gin.Context) {
-	if err := deleteTodoByID(c.Param("id")); err != nil {
-		c.JSON(500, gin.H{"error": "Internal server error"})
-		return
+	if deleteTodoByID(c.Param("id")) {
+		c.JSON(200, gin.H{"status": "Todo deleted"})
+	} else {
+		c.JSON(404, gin.H{"error": "Todo not found"})
 	}
-	c.JSON(200, gin.H{"status": "Todo deleted"})
 }
 
-func deleteTodoByID(id string) error {
-	_, err := db.Exec("DELETE FROM todos WHERE todo_id = ?", id)
-	return err
+func deleteTodoByID(id string) bool {
+	todoLock.Lock()
+	defer todoLock.Unlock()
+
+	idInt, err := strconv.Atoi(id)
+	if err != nil {
+		return false
+	}
+
+	for index, todo := range todos {
+		if todo.ID == idInt {
+			todos = append(todos[:index], todos[index+1:]...)
+			return true
+		}
+	}
+
+	return false
 }
